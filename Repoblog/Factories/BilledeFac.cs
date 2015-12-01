0b177fb9efaf0fd3599e77c20c99eea22800ce65
Rikerdude <riker789@gmail.com>
2015-12-01 09:33:34 +0100

    A simple Blog made in MVC not done yet :)

---
+++ Repoblog/Factories/BilledeFac.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repoblog.Models.BaseModels;
+using REPO_NAMESPACE;
+
+namespace Repoblog.Factories
+{
+    public class BilledeFac : AutoFac<Billede>
+    {
+
+    }
+}
---
+++ Repoblog/Models/ViewModels/BlogIndex.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repoblog.Models.BaseModels;
+
+namespace Repoblog.Models.ViewModels
+{
+    public class BlogIndex
+    {
+        public Post Post { get; set; }
+        public List<Billede> Billeder { get; set; } 
+    }
+}
